dcc65c2be9ae94bc6f58a6df072f9c1666a2723a
Dillon <dsharlet@gmail.com>
2015-01-27 00:09:17 -0800

    Fixed leak of directory enumeration, renamed attributes name -> driver_name, dp -> decimals

--- ev3dev.cpp
+++ ev3dev.cpp
@@ -175,7 +175,7 @@ bool device::connect(const std::string &dir,
                                     std::set<std::string>> &match) noexcept
 {
   using namespace std;
-  
+
   const size_t pattern_length = pattern.length();
   
   struct dirent *dp;
@@ -206,8 +206,10 @@ bool device::connect(const std::string &dir,
             }
           }
           
-          if (bMatch)
+          if (bMatch) {
+            closedir(dfd);
             return true;
+          }
         }
         catch (...) { }
         
@@ -442,14 +444,14 @@ sensor::sensor(port_type port)
 sensor::sensor(port_type port, const std::set<sensor_type> &types)
 {
   connect({{ "port_name", { port }},
-           { "name",        types }});
+           { "driver_name", types }});
 }
 
 //-----------------------------------------------------------------------------
 
 bool sensor::connect(const std::map<std::string, std::set<std::string>> &match) noexcept
 {
-  static const std::string _strClassDir { SYS_ROOT "/class/msensor/" };
+  static const std::string _strClassDir { SYS_ROOT "/class/lego-sensor/" };
   static const std::string _strPattern  { "sensor" };
   
   try
@@ -542,13 +544,13 @@ void sensor::init_members(bool bAll)
   if (bAll)
   {
     _port_name = get_attr_string("port_name");
-    _type = get_attr_string("name");
+    _type = get_attr_string("driver_name");
   }
 
   _mode    = get_attr_string("mode");
   _modes   = get_attr_set("modes");
   _nvalues = get_attr_int("num_values");
-  _dp      = get_attr_int("dp");
+  _dp      = get_attr_int("decimals");
   
   _dp_scale = 1.f;
   for (unsigned dp = _dp; dp; --dp)
@@ -580,7 +582,7 @@ i2c_sensor::i2c_sensor(port_type port_) :
 i2c_sensor::i2c_sensor(port_type port_, address_type address_)
 {
   connect({{ "port_name", { port_ }},
-           { "name",      { nxt_i2c_sensor }},
+           { "driver_name",      { nxt_i2c_sensor }},
            { "address",   { address_ }}});
 }
 
