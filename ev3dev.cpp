d17d795882980ec4f1d1beb903eaaca24c5fd8f8
Denis Demidov <dennis.demidov@gmail.com>
2015-05-21 21:54:35 +0300

    [cpp] Well-known sensors use informative names in the latest kernel
    See ev3dev/lego-linux-drivers@ba2d36f7171fe26e31a896533f455786788bf90c

--- ev3dev.cpp
+++ ev3dev.cpp
@@ -429,10 +429,10 @@ std::string device::get_attr_from_set(const std::string &name) const
 //-----------------------------------------------------------------------------
 
 const sensor::sensor_type sensor::ev3_touch       { "lego-ev3-touch" };
-const sensor::sensor_type sensor::ev3_color       { "lego-ev3-uart-29" };
-const sensor::sensor_type sensor::ev3_ultrasonic  { "lego-ev3-uart-30" };
-const sensor::sensor_type sensor::ev3_gyro        { "lego-ev3-uart-32" };
-const sensor::sensor_type sensor::ev3_infrared    { "lego-ev3-uart-33" };
+const sensor::sensor_type sensor::ev3_color       { "lego-ev3-color" };
+const sensor::sensor_type sensor::ev3_ultrasonic  { "lego-ev3-us" };
+const sensor::sensor_type sensor::ev3_gyro        { "lego-ev3-gyro" };
+const sensor::sensor_type sensor::ev3_infrared    { "lego-ev3-ir" };
 
 const sensor::sensor_type sensor::nxt_touch       { "lego-nxt-touch" };
 const sensor::sensor_type sensor::nxt_light       { "lego-nxt-light" };
